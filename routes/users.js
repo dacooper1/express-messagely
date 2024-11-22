50f83437e44ade92fd8b6c2738463ed548435575
Dejah Cooper <Dejah.C@icloud.com>
2024-11-22 14:20:39 -0500

    added ensureLoggedIn middleware to GET /users route

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ const router = new express.Router();
  *
  **/
 
-router.get("/", async function(req, res, next) {
+router.get("/", ensureLoggedIn, async function(req, res, next) {
     const results = await User.all()
 
     return res.json({user: results})
